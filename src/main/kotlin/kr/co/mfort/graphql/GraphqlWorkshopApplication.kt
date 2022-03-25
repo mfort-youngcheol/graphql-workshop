d994da1e0aba334619b0620cfc81cde14b97ffc2
mfort-youngcheol <yc.son@mfort.co.kr>
2022-03-25 15:12:23 +0900

    init project

---
+++ src/main/kotlin/kr/co/mfort/graphql/GraphqlWorkshopApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.mfort.graphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlWorkshopApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlWorkshopApplication>(*args)
+}
---
+++ src/test/kotlin/kr/co/mfort/graphql/GraphqlWorkshopApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.mfort.graphql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlWorkshopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
